7fa5bcd3a8adcde37f9bdf6595e406b62a8b37e2
terrence-ou <outerrencedl@gmail.com>
2024-09-01 14:35:29 -0400

    claude documentation updated

--- src/libs/claude.ts
+++ src/libs/claude.ts
@@ -32,14 +32,20 @@ export class Claude {
 
     this.headers = {
       "x-api-key": this.api,
-      "anthropic-version": "2023-06-01",
+      "anthropic-version": "2023-06-01", // This is the current version provided by the Claude
       "Content-Type": "application/json",
     };
   }
 
   /*
-   * Generates a response from Claude
-   * @param system: string.
+   * Generates a response from Claude.
+   * @param system: string - The system prompt or context that guides the model's behavior.
+   * @param messages: ClaudeMessage[] - An array of ClaudeMessages representing the conversation history.
+   * @param temperature (Optional): number - A number between 0 and 1 that controls the randomness of the output. Defaults to 0.
+   * @param max_tokens (Optional): number - A number represents the maximum output tokens (words or word fragments). Defaults to 1000.
+   *
+   * @returns A promise that resolves to a ClaudeResult containing the generated text result.
+   * @throws An error is the API request fails.
    */
   async generate(
     system: string,
@@ -47,10 +53,14 @@ export class Claude {
     temperature: number = 0,
     max_tokens: number = 1000,
   ): Promise<ClaudeResult> {
+    // Limits the temperature between 0.0 and 1.0
+    const validatedTemp = Math.min(1.0, Math.max(0.0, temperature));
+    // Limits the minimum max_token to 0
+    const validatedToken = Math.max(0, Math.round(max_tokens));
     const data: ClaudeInputData = {
       model: this.model,
-      max_tokens,
-      temperature,
+      max_tokens: validatedToken,
+      temperature: validatedTemp,
       system,
       messages,
     };
